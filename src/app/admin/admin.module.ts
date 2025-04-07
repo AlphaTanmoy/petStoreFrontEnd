a5879503f5be768517d99d58539d9ea156a2ae85
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-07 21:59:38 +0530

    updated - admin service

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { AdminListComponent } from './admin-list/admin-list.component';
+
+@NgModule({
+  imports: [
+    AdminListComponent
+  ]
+})
+export class AdminModule { }
