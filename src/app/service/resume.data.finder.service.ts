7f7a6e6c1ea793d3d0b64e395fb917dd60c68fb8
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-23 22:19:30 +0530

    hided api key

---
+++ src/app/service/resume.data.finder.service.ts
@@ -0,0 +1,57 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { API_KEYS } from '../utils/apikey.secrect'
+@Injectable({
+  providedIn: 'root'
+})
+export class ResumeDataFinderService {
+
+  private readonly GITHUB_API = 'https://api.github.com/graphql';
+  private readonly TOKEN = API_KEYS.github_token;
+  private userName = 'AlphaTanmoy'
+  constructor(private http: HttpClient) { }
+
+
+  getGithubData(): Observable<any> {
+    const query = `
+      query {
+        user(login: "${this.userName}") {
+          contributionsCollection {
+            contributionCalendar {
+              totalContributions
+            }
+          }
+          repositories(first: 100, ownerAffiliations: OWNER, isFork: false) {
+            nodes {
+              name
+              isPrivate
+              stargazerCount
+              languages(first: 10, orderBy: {field: SIZE, direction: DESC}) {
+                edges {
+                  size
+                  node {
+                    name
+                    color
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `;
+
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${this.TOKEN}`,
+      'Content-Type': 'application/json'
+    });
+
+    return this.http.post(this.GITHUB_API, { query }, { headers });
+  }
+
+  getLeetcodeData(): Observable<any> {
+    return this.http.get(`https://leetcode-stats-api.herokuapp.com/Alpha_Tanmoy`);
+  }
+
+}
