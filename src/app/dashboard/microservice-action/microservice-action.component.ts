ea0d372c70ef67b078f8e095cdedaf3a091f0f60
alphaTanmoy <dtanmoy169@gmail.com>
2025-04-27 03:55:37 +0530

    backend need to config frontend done for this

--- src/app/dashboard/microservice-action/microservice-action.component.ts
+++ src/app/dashboard/microservice-action/microservice-action.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { MICROSERVICE_NAME } from '../../constants/Enums';
 import { CommonModule } from '@angular/common';
+import { GetAPIEndpoint } from '../../constants/endpoints';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-microservice-action',
@@ -14,30 +16,28 @@ export class MicroserviceActionComponent {
 
   microservices = Object.values(MICROSERVICE_NAME);
   isLoading = false;
+  private api_endpoint = GetAPIEndpoint(MICROSERVICE_NAME.CORE, '/microservice');
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private snackBar: MatSnackBar) {}
 
   handleAction(service: string, action: 'start' | 'stop' | 'restart') {
     this.isLoading = true;
+    let url = `${this.api_endpoint}/${action}/${service}`;
 
-    let url = '';
-    if (action === 'start') {
-      url = `/microservice/start/${service}`;
-    } else if (action === 'stop') {
-      url = `/microservice/stop/${service}`;
-    } else if (action === 'restart') {
-      url = `/microservice/restart/${service}`;
-    }
-
-    // Now call your API using HttpClient
-    this.http.get(url).subscribe({
+    this.http.post(url, {}).subscribe({
       next: (response) => {
-        console.log(`${action} successful`, response);
         this.isLoading = false;
+        this.snackBar.open(`${action} successful for ${service}`, 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-success']
+        });
       },
       error: (error) => {
-        console.error(`${action} failed`, error);
         this.isLoading = false;
+        this.snackBar.open(`${action} failed for ${service}`, 'Close', {
+          duration: 3000,
+          panelClass: ['snackbar-error']
+        });
       }
     });
   }
